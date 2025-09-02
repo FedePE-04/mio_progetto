10079d351331227fe99c5d8aac516a58baf1aae8
Fede-PE04 <fpavone@itsmeccanicabruzzo.eu>
2025-09-02 11:34:06 +0200

    cartella con file essenziali

--- task2_catalogo/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-    public static void main(String []args) {
-        Prodotto p1 = new Prodotto("Scarpa", 20);
-        p1.calcolaPrezzoFinale( 5);
-        ProdottoAlimentare p2 = new ProdottoAlimentare("Pesca",5);
-        p2.calcolaPrezzoFinale(5);
-    }
-}
--- task2_java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Prodotto p1 = new Prodotto("scarpa",20);
-        p1.calcolaPrezzoFinale(10);
-}
-}
