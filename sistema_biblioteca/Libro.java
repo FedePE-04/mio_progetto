9c8510bae857e49aafdf522463cd8500d1aa06ad
Fede-PE04 <fpavone@itsmeccanicabruzzo.eu>
2025-09-02 11:21:03 +0200

    progetto svolto a scuola

---
+++ sistema_biblioteca/Libro.java
@@ -0,0 +1,18 @@
+
+
+public class Libro {
+    private String Titolo;
+    private String Autore;
+    private int Anno;
+
+
+    public Libro(String Titolo, String Autore, int Anno){
+        this.Titolo = Titolo;
+        this.Autore = Autore;
+        this.Anno = Anno;
+    }
+
+public void aggiungereLibro(){
+
+}
+}
